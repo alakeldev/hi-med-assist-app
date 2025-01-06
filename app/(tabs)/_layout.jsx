83597e3f630979c3a70953ac1ceebb30dfaa0888
Abdullah Alakel <abdullah.k.alakel@gmail.com>
2025-01-06 17:50:36 +0100

    edit main _layout.jsx 'add tabBarLabel + add tabBarIcon + Import MaterialIcons to dispaly the icons'

--- app/(tabs)/_layout.jsx
+++ app/(tabs)/_layout.jsx
@@ -1,15 +1,38 @@
 import { View, Text } from 'react-native'
 import React from 'react'
 import { Tabs } from 'expo-router';
+import MaterialIcons from '@expo/vector-icons/MaterialIcons';
 
 export default function TabLayout() {
   return (
     <Tabs screenOptions={{
         headerShown: false
     }}>
-        <Tabs.Screen name='index' />
-        <Tabs.Screen name='AddMed' />
-        <Tabs.Screen name='MyProfile' />
+        <Tabs.Screen name='index' 
+        options={{
+            tabBarLabel: "Home",
+            tabBarIcon:({color, size}) => (
+                <MaterialIcons name="medical-information" size={size} color={color} />
+            )
+        }}
+        />
+        <Tabs.Screen name='AddMed' 
+        options={{
+            tabBarLabel: "Add Med",
+            tabBarIcon:({color, size}) => (
+                <MaterialIcons name="notification-add" size={size} color={color} />
+            )
+        }}
+        
+        />
+        <Tabs.Screen name='MyProfile' 
+        options={{
+            tabBarLabel: "Account",
+            tabBarIcon:({color, size}) => (
+                <MaterialIcons name="tag-faces" size={size} color={color} />
+            )
+        }}
+        />
     </Tabs>
   )
 }
