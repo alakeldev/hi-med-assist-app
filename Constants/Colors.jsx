a1804f24166cd784244f0a09a20235a717c7aacd
Abdullah Alakel <abdullah.k.alakel@gmail.com>
2025-01-06 21:11:24 +0100

    create login dir + its index.jsx 'as intro page for my application initiate its structure and styles + add image intro' + edit (tabs)/index.jsx 'added redirect router to the login intro page' + edit app/_layout.jsx 'added login to its stack expo-router'

---
+++ Constants/Colors.jsx
@@ -0,0 +1,4 @@
+export default {
+    MAIN: "#ececec",
+    LOGIN: "#001932",
+}
---
+++ app/login/index.jsx
@@ -0,0 +1,70 @@
+import { View, Text, Image, StyleSheet, TouchableOpacity } from 'react-native';
+import React from 'react';
+import Colors from '../../Constants/Colors';
+
+export default function index() {
+  return (
+    <View style = {{
+      backgroundColor: Colors.MAIN,
+    }}>
+      <View style={styles?.imageContainer}>
+        <Image source={require('./../../assets/images/intro.png')} 
+          style={styles?.image}
+        />
+      </View>
+
+      <View style={styles?.headerTextContainer}>
+        <Text style={styles?.headerText}>Organise Your Medications Efficiently!</Text>
+        <Text style={styles?.subHeaderText}>Stay on track with your health by effortlessly scheduling and monitoring your medication regimen</Text>
+        <TouchableOpacity style={styles?.button}>
+          <Text style={styles?.buttonText}>Login / Register</Text>
+        </TouchableOpacity>
+      </View>
+    </View>
+  )
+}
+
+const styles = StyleSheet.create({
+  imageContainer: {
+    display:"flex",
+    marginTop: 50,
+    alignItems: "center",
+    marginBottom: 10,
+    
+  },
+  image: {
+    width: 240,
+    height: 400,
+    borderRadius: 25,
+  },
+  headerTextContainer: {
+    height: "100%",
+    padding: 30,
+    backgroundColor: Colors.LOGIN,
+    
+  },
+  headerText: {
+    fontSize: 25,
+    fontWeight: "bold",
+    textAlign: "center",
+    color: "white",
+  },
+  subHeaderText: {
+    textAlign: "center",
+    fontSize: 18,
+    marginTop: 15,
+    color: "white",
+  },
+  button: {
+    padding: 20,
+    backgroundColor: Colors.MAIN,
+    borderRadius: 50,
+    marginTop: 20,
+
+  },
+  buttonText: {
+    textAlign: "center",
+    fontSize: 18,
+    fontWeight: "bold"
+  }
+})
